5bfbca89a45e67eff0290808c824846fd1b95bac
Fred Bricon <fbricon@gmail.com>
2011-11-14 14:42:36 +0100

    Use proper junit assertions in tests
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

--- jboss-as-poh5-archetype/src/main/resources/archetype-resources/src/test/java/test/MemberRegistrationTest.java
+++ jboss-as-poh5-archetype/src/main/resources/archetype-resources/src/test/java/test/MemberRegistrationTest.java
@@ -3,15 +3,15 @@
 #set( $symbol_escape = '\' )
 package ${package}.test;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
-import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
 
 import javax.inject.Inject;
 import javax.ws.rs.core.Response;
 
-import ${package}.rest.MemberService;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.Archive;
@@ -21,6 +21,7 @@ import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import ${package}.rest.MemberService;
 import ${package}.model.Member;
 import ${package}.util.Resources;
 
@@ -44,7 +45,7 @@ public class MemberRegistrationTest {
    public void testRegister() throws Exception {
       Response response = memberRegistration.createMember("Jane Doe", "jane@mailinator.com", "2125551234");
 
-      assertTrue(response.getStatus() == 200);
+      assertEquals("Unexpected response status", 200, response.getStatus());
       log.info(" New member was persisted and returned status " + response.getStatus());
    }
 
@@ -52,9 +53,9 @@ public class MemberRegistrationTest {
    public void testInvalidRegister() throws Exception {
       Response response = memberRegistration.createMember("", "", "");
 
-      assertTrue(response.getStatus() == 400);
-      assertTrue(response.getEntity() != null);
-      assertTrue(((HashMap<String, String>)response.getEntity()).size() == 3);
+      assertEquals("Unexpected response status", 400, response.getStatus());
+      assertNotNull("response.getEntity() should not null",response.getEntity());
+      assertEquals("Unexpected response.getEntity(). It contains " + response.getEntity(), 2, ((Map<String, String>)response.getEntity()).size());
       log.info("Invalid member register attempt failed with return code " + response.getStatus());
    }
 
@@ -66,10 +67,9 @@ public class MemberRegistrationTest {
       //Register a different user with the same email
       Response response = memberRegistration.createMember("John Doe", "jane@mailinator.com", "2133551234");
 
-      assertTrue(response.getStatus() == 409);
-      assertTrue(response.getEntity() != null);
-      assertTrue(((HashMap<String, String>)response.getEntity()).size() == 1);
+      assertEquals("Unexpected response status", 409, response.getStatus());
+      assertNotNull("response.getEntity() should not null",response.getEntity());
+      assertEquals("Unexpected response.getEntity(). It contains" + response.getEntity(), 1, ((Map<String, String>)response.getEntity()).size());
       log.info("Duplicate member register attempt failed with return code " + response.getStatus());
    }
-   
 }
