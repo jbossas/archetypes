10c98d72d03f21edd43e76fe5d44eceaeb8706a4
Rafael Benevides <rafabene@gmail.com>
2013-01-17 11:13:50 -0200

    Update blank archetypes

--- jboss-javaee6-webapp-blank-archetype/src/main/resources/archetype-resources/src/main/java/controller/MemberController.java
+++ jboss-javaee6-webapp-blank-archetype/src/main/resources/archetype-resources/src/main/java/controller/MemberController.java
@@ -41,13 +41,39 @@ public class MemberController {
    }
 
    public void register() throws Exception {
-      memberRegistration.register(newMember);
-      facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Registered!", "Registration successful"));
-      initNewMember();
+       try {
+           memberRegistration.register(newMember);
+           FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_INFO, "Registered!", "Registration successful");
+           facesContext.addMessage(null, m);
+           initNewMember();
+       } catch (Exception e) {
+           String errorMessage = getRootErrorMessage(e);
+           FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_ERROR, errorMessage, "Registration unsuccessful");
+           facesContext.addMessage(null, m);
+       }
    }
 
    @PostConstruct
    public void initNewMember() {
       newMember = new Member();
+   }
+   
+   private String getRootErrorMessage(Exception e) {
+       // Default to general error message that registration failed.
+       String errorMessage = "Registration failed. See server log for more information";
+       if (e == null) {
+           // This shouldn't happen, but return the default messages
+           return errorMessage;
+       }
+
+       // Start with the exception and recurse to find the root cause
+       Throwable t = e;
+       while (t != null) {
+           // Get the message from the Throwable class instance
+           errorMessage = t.getLocalizedMessage();
+           t = t.getCause();
+       }
+       // This is the root cause message
+       return errorMessage;
    }
 }
