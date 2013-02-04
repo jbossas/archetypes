70e0fb7b4f306a027765147f6fb99583a03169f1
Rafael Benevides <rafabene@gmail.com>
2013-02-04 10:47:14 -0500

    Correct Karel's email

--- release.sh
+++ release.sh
@@ -17,7 +17,7 @@ DIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 # EAP team email subject
 EMAIL_SUBJECT="\${RELEASEVERSION} of JBoss AS Archetypes released, please merge with https://github.com/jboss-eap/archetypes, tag and add to EAP maven repo build"
 # EAP team email To ?
-EMAIL_TO="pgier@redhat.com kpwiko@redhat.com"
+EMAIL_TO="pgier@redhat.com kpiwko@redhat.com"
 EMAIL_FROM="\"JDF Publish Script\" <benevides@redhat.com>"
 
 
